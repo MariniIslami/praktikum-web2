b45f648b8b533ce3a86782c60da6b229ca10e129
MariniIslami <mariniislami24@gmail.com>
2024-06-05 13:38:10 +0800

    Initial commit

---
+++ profile.php
@@ -0,0 +1,45 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/marini.jpg" alt="Marini Islami" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NIK / NPM :</strong></td>
+                        <td align="left" width="50%">20 0403 0393 / 2210010690</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studi :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Marini Islami</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Banjarmasin, 24 Maret 2004</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">0813 4916 7667</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">mariniislami24@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="https://www.instagram.com/mariniislami_?igsh=Ync2b2d6dnZjNDJs&utm_source=qr" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/instagram.svg" width="30" alt="instagram">
+                            </a>
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<?php include "footer.php" ?>
---
+++ test.php
@@ -0,0 +1,2 @@
+<?php
+echo password_hash("rini", PASSWORD_DEFAULT);
